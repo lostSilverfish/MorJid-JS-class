f0cb874ea53f7261443cef22a3c0172a9959f783
Lost Silverfish <lostSilverfish21@gmail.com>
2023-07-29 19:51:12 +0500

    calc finished

--- Day 3/index.js
+++ Day 3/index.js
@@ -1,1 +1,41 @@
 "use strict";
+
+const display = document.getElementById("display");
+const numberPad = document.querySelectorAll(".number");
+const clearBtn = document.getElementById("clear");
+const equalBtn = document.getElementById("equal");
+
+let displayText = "";
+
+// helper functions
+function addInputs(input) {
+  displayText += input;
+  display.textContent = displayText;
+}
+
+function clearDisplay() {
+  display.textContent = "00";
+  displayText = "";
+}
+
+function evaluate() {
+  let result = eval(displayText);
+  displayText = result;
+  display.textContent = displayText;
+}
+
+// Adding Event Listeners
+for (let i = 0; i < numberPad.length; i++) {
+  numberPad[i].addEventListener("click", function () {
+    const input = numberPad[i].textContent;
+    addInputs(input);
+  });
+}
+
+clearBtn.addEventListener("click", function () {
+  clearDisplay();
+});
+
+equalBtn.addEventListener("click", function () {
+  evaluate();
+});
