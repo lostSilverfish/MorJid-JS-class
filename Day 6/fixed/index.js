66a370b3f9f5d3cfcd0e34dbaf02c803c59cc6da
Lost Silverfish <lostSilverfish21@gmail.com>
2023-08-27 20:00:44 +0500

    day 6 project

---
+++ Day 6/fixed/index.js
@@ -0,0 +1,97 @@
+"use strict";
+
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 720;
+canvas.height = 500;
+
+const gravity = 1;
+
+const circle = {
+  radius: 10,
+  pos: {
+    x: canvas.width / 2,
+    y: canvas.height - 10,
+  },
+  color: "#ba5a31",
+  velocity: {
+    x: 0,
+    y: 0,
+  },
+  speed: 20,
+  jumping: false,
+  draw() {
+    ctx.fillStyle = circle.color;
+    ctx.beginPath();
+    ctx.arc(circle.pos.x, circle.pos.y, circle.radius, 0, Math.PI * 2);
+    ctx.fill();
+  },
+  move() {
+    circle.checkXBoundry();
+
+    circle.pos.x += circle.velocity.x;
+    circle.pos.y += circle.velocity.y;
+
+    circle.fall();
+  },
+  jump() {
+    if (!circle.jumping) {
+      circle.jumping = true;
+      circle.velocity.y = -circle.speed;
+    }
+  },
+  fall() {
+    if (circle.jumping && circle.pos.y < canvas.height - circle.radius) {
+      circle.velocity.y += gravity;
+    } else {
+      circle.velocity.y = 0;
+      circle.jumping = false;
+    }
+  },
+  moveLeft() {
+    circle.velocity.x = -circle.speed / 4;
+  },
+  moveRight() {
+    circle.velocity.x = circle.speed / 4;
+  },
+  checkXBoundry() {
+    if (circle.pos.x < circle.radius) {
+      circle.pos.x = circle.radius;
+    }
+
+    if (circle.pos.x > canvas.width - circle.radius) {
+      circle.pos.x = canvas.width - circle.radius;
+    }
+  },
+};
+
+function animate() {
+  ctx.reset();
+  circle.draw();
+  circle.move();
+
+  requestAnimationFrame(animate);
+}
+
+animate();
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === " ") {
+    circle.jump();
+  }
+
+  if (e.key === "ArrowLeft") {
+    circle.moveLeft();
+  }
+
+  if (e.key === "ArrowRight") {
+    circle.moveRight();
+  }
+});
+
+document.addEventListener("keyup", function (e) {
+  if (e.key === "ArrowLeft" || e.key === "ArrowRight") {
+    circle.velocity.x = 0;
+  }
+});
